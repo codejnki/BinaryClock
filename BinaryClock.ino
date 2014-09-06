350d914428bc41854178a94e12ed004cb29232d0
Patrick Stockton <patrick@codejnki.com>
2014-09-06 16:09:57 -0400

    pulse led to conserve power

--- BinaryClock.ino
+++ BinaryClock.ino
@@ -11,6 +11,13 @@ int hourLightArray[32] = {0};
 int minuteLeds[6] = { 32 , 30, 28, 26, 24, 22 };
 int minuteLightArray[32] = {0};
 
+// variables to hold current time to try and be a little
+// more power efficient
+int currentMinute = 0;
+int currentHour = 0;
+long previousMillis = 0;
+long interval = 1;
+
 // Create our RTC class
 RTC_DS1307 RTC;
 
@@ -44,30 +51,69 @@ void loop()
 
 	// The clock currently reads in 12 hour format
 	// change here to go to 24 hour format
+	if (hour == 0)
+	{
+		hour = 12;
+	}
+
 	if (hour >= 13)
 	{
 		hour = hour - 12;
 	}
-		
-	convertToBitArray(hourLightArray, hour);
-	convertToBitArray(minuteLightArray, minute);
 
-	// so we converted the minutes in to a 32 bit length array
-	// but we only are going to need the last 6 array elements
-	int lightCounter = 26;
-	for (int i = 0; i < 6; i++)
+	if (currentHour != hour)
 	{
-		digitalWrite(minuteLeds[i],minuteLightArray[lightCounter]);
-		lightCounter++;
+		// update the current hour and then update the LED array
+		currentHour = hour;
+		convertToBitArray(hourLightArray, currentHour);
 	}
 
-	// we converted the hours in to a 32 bit length array
-	// but we are only going to need the last 4 array elements
-	lightCounter = 28;
-	for (int i = 0; i < 4; i++)
+	if (currentMinute != minute)
 	{
-		digitalWrite(hourLeds[i], hourLightArray[lightCounter]);
-		lightCounter++;
+		// update the current min and then updat the LED array
+		currentMinute = minute;
+		convertToBitArray(minuteLightArray, minute);
+	}
+
+	unsigned long currentMillis = millis();
+	if(currentMillis - previousMillis > interval)
+	{
+		previousMillis = currentMillis;
+		// so we converted the minutes in to a 32 bit length array
+		// but we only are going to need the last 6 array elements
+		int lightCounter = 26;
+		for (int i = 0; i < 6; i++)
+		{
+			digitalWrite(minuteLeds[i],minuteLightArray[lightCounter]);
+			lightCounter++;
+		}
+
+		// we converted the hours in to a 32 bit length array
+		// but we are only going to need the last 4 array elements
+		lightCounter = 28;
+		for (int i = 0; i < 4; i++)
+		{
+			digitalWrite(hourLeds[i], hourLightArray[lightCounter]);
+			lightCounter++;
+		}
+	}
+	else
+	{
+		int lightCounter = 26;
+		for (int i = 0; i < 6; i++)
+		{
+			digitalWrite(minuteLeds[i],LOW);
+			lightCounter++;
+		}
+
+		// we converted the hours in to a 32 bit length array
+		// but we are only going to need the last 4 array elements
+		lightCounter = 28;
+		for (int i = 0; i < 4; i++)
+		{
+			digitalWrite(hourLeds[i], LOW);
+			lightCounter++;
+		}
 	}
 }
 
